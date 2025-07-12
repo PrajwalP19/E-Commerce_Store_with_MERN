4b2db37a7b17488ffdb1d8c307725578fee39c7f
Prajwal Patil <ppbaviskar7030@gmail.com>
2025-07-12 20:25:10 +0530

    added stripe model and controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 
@@ -19,10 +20,18 @@ app.use(express.json())   //allows to parse the body of req
 app.use(cookieParser())   
 
 
+
 app.use("/api/v1/auth", authRoutes)
+
 app.use("/api/v1/products", productRoutes)
+
 app.use("/api/v1/cart", cartRoutes)
+
 app.use("/api/v1/coupons", couponRoutes)
+
+app.use("/api/v1/payments", paymentRoutes)
+
+
 
 app.listen(5000, () => {
     console.log(`server is listening on http://localhost:${PORT}`)
